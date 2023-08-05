409447598b81ec91aadd231b5dc0bd5692df95a2
Bboy010 <hkoffianderson@gmail.com>
2023-08-04 19:37:53 -0700

    Basic annotations -concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,8 +2,8 @@
 '''concat string
 '''
 
+
 def concat(str1: str, str2: str) -> str:
-    '''
-    Return a concatenate string with string 1 and 2.
+    '''Return a concatenate string with string 1 and 2.
     '''
     return str1 + str2
